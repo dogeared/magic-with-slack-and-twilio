6598f2051fac0b05704b145fe9e26f431046950e
Micah Silverman <micah@afitnerd.com>
2017-09-28 23:56:27 -0400

    Fix LSP

--- magic-api/src/main/java/com/afitnerd/magic/model/slack/SlackInChannelImageResponse.java
+++ magic-api/src/main/java/com/afitnerd/magic/model/slack/SlackInChannelImageResponse.java
@@ -1,20 +1,42 @@
 package com.afitnerd.magic.model.slack;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SlackInChannelImageResponse extends SlackResponse {
 
+    private List<Attachment> attachments = new ArrayList<>();
+
     public SlackInChannelImageResponse(String imageUrl) {
-        getAttachments().add(new Attachment(imageUrl));
+        attachments.add(new Attachment(imageUrl));
     }
 
-    @Override
-    public String getText() {
-        return null;
+    public List<Attachment> getAttachments() {
+        return attachments;
     }
 
     @Override
     public String getResponseType() {
         return "in_channel";
+    }
+
+    public class Attachment {
+
+        @JsonProperty("image_url")
+        private String imageUrl;
+
+        public Attachment(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
     }
 }
--- magic-api/src/main/java/com/afitnerd/magic/model/slack/SlackResponse.java
+++ magic-api/src/main/java/com/afitnerd/magic/model/slack/SlackResponse.java
@@ -1,42 +1,9 @@
 package com.afitnerd.magic.model.slack;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class SlackResponse {
 
-    private List<Attachment> attachments = new ArrayList<>();
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public List<Attachment> getAttachments() {
-        return attachments;
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public abstract String getText();
-
     @JsonProperty("response_type")
     public abstract String getResponseType();
-
-
-    public class Attachment {
-
-        @JsonProperty("image_url")
-        private String imageUrl;
-
-        public Attachment(String imageUrl) {
-            this.imageUrl = imageUrl;
-        }
-
-        public String getImageUrl() {
-            return imageUrl;
-        }
-
-        public void setImageUrl(String imageUrl) {
-            this.imageUrl = imageUrl;
-        }
-    }
 }
